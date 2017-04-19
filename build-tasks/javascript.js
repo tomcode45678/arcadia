501d5626edeafe824c7e7bb010daaa501dd0a531
tomsta93 <thomasstapleton0@gmail.com>
2017-04-19 22:04:48 +0100

    Removed logic for multiple module loaders.

--- build-tasks/javascript.js
+++ build-tasks/javascript.js
@@ -9,38 +9,21 @@ module.exports = function (gulp, tools, defaultTasks, env) {
     env = 'development';
   }
 
-  let output = tools.gutil.env.output;
-  if (!output) {
-    output = 'systemjs'; // default
-  }
-
   const PATHS = {
     src: 'src/**/*.js',
     browserSupport: 'src/**/browser-support/*.js'
-  }
+  };
 
   const babel = require('gulp-babel');
-  const browserify = require('browserify');
   const DIST = 'dist';
 
   // Any babel config
-  let babelConfig = {
-    presets: ['es2015'],
-    plugins: []
+  const babelConfig = {
+    moduleIds: true,
+    plugins: ['transform-es2015-modules-systemjs']
   };
 
   let buildDeps = ['esl'];
-
-  if (output === 'systemjs' || output === 'amd') {
-    Object.assign(babelConfig, {
-      moduleIds: true
-    });
-    babelConfig.plugins.push(`transform-es2015-modules-${output}`);
-  }
-  else if (output === 'commonjs') {
-    buildDeps.push('commonjs');
-    babelConfig.plugins.push('transform-es2015-modules-commonjs');
-  }
 
   function compile (watch) {
     return gulp.src([PATHS.src, `!${PATHS.browserSupport}`])
@@ -48,7 +31,7 @@ module.exports = function (gulp, tools, defaultTasks, env) {
       .pipe(watch ? tools.plumber() : tools.gutil.noop())
       .pipe(tools.sourcemaps.init())
       .pipe(babel(babelConfig))
-      .pipe(output !== 'commonjs' ? tools.concat('util.js') : tools.gutil.noop())
+      .pipe(tools.concat('util.js'))
       .pipe(env === 'production' ? tools.uglify() : tools.gutil.noop())
       .pipe(tools.sourcemaps.write('.'))
       .pipe(gulp.dest(DIST))
@@ -59,15 +42,6 @@ module.exports = function (gulp, tools, defaultTasks, env) {
   gulp.task('esl', compile);
 
   gulp.task('watch-esl', () => gulp.watch(PATHS.src, ['esl']));
-
-  gulp.task('commonjs', () => {
-    return browserify(`${DIST}/main.js`)
-      .bundle()
-      .on('error', e => tools.gutil.log(e))
-      .pipe(tools.source('index.js'))
-      .pipe(gulp.dest('examples/commonjs')
-    );
-  });
 
   // For IE8+ support
   gulp.task('browser-support', () => {
